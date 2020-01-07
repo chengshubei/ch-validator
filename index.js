d988f4660483e9e6ea7d34a9f1340096379e1671
chengshubei <csb@disanbo.com>
2020-01-07 16:59:03 +0800

    优化格式化和检测顺序

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const rules = require('./rules');
 const language = require('./lang');
 
 /**
- * ruleMap.set('/user/info', {'id': {
+ * ruleMap.set('get_/user/info', {'id': {
  *      en: 'id',               //英文参数名
  *      zh_CN: '用户编号',      //中文参数名
  *      rules: [{
@@ -19,8 +19,8 @@ let ruleMap = new Map();
 
 //使用说明：第一个规则，必须是字段名
 module.exports = (ruleObj = {}) => {
-    for (let path in ruleObj) {
-        let params = ruleObj[path];
+    for (let uri in ruleObj) {
+        let params = ruleObj[uri];
         let options = {};
         for (let key in params) {
             if (! Array.isArray(params[key]) || params[key].length <= 1) continue;
@@ -34,7 +34,9 @@ module.exports = (ruleObj = {}) => {
                 //校验规则条件
                 if (option) rules[rule](0, option);
                 let vl = {rule, option};
-                if (['required', 'required_if', 'required_unless', 'default'].includes(rule)) ruleList.unshift(vl);
+                //优先检测条件
+                let preRules = ['required', 'required_if', 'required_unless', 'default', 'string', 'numeric', 'integer'];
+                if (preRules.includes(rule)) ruleList.unshift(vl);
                 else ruleList.push(vl);
             }
             options[key] = {
@@ -43,7 +45,7 @@ module.exports = (ruleObj = {}) => {
                 rules: ruleList
             };
         }
-        ruleMap.set(path, options);
+        ruleMap.set(uri, options);
     }
 
     return function (ctx, next) {
@@ -55,14 +57,15 @@ module.exports = (ruleObj = {}) => {
             let keyMap = ruleMap.get(uri);
             for (let key in keyMap) {
                 let comment = lang === 'en' ? keyMap[key]['en'] : keyMap[key]['zh_CN'];
+                let value = attributes[key];
                 for (let {rule, option} of keyMap[key]['rules']) {
-                    let result = rules[rule](attributes[key], option, attributes);
+                    let result = rules[rule](value, option, attributes);
                     if (! result[0]) {
                         let err = `${comment}${language[lang][rule]}`;
                         if (err.charAt(err.length - 1) === ' ') err += String(option);
                         throw new ValidateError(err);
                     } else if (result[1] !== null) {
-                        params[key] = result[1];
+                        params[key] = value = result[1];
                     }
                 }
             }
