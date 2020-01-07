7a6571b205cb03ff99415c8552740a9fa036dc5e
chengshubei <csb@disanbo.com>
2020-01-07 17:33:04 +0800

    修改检测策略

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const language = require('./lang');
  * ruleMap.set('get_/user/info', {'id': {
  *      en: 'id',               //英文参数名
  *      zh_CN: '用户编号',      //中文参数名
- *      rules: [{
+ *      necessity: [{          //参数必要性检测集合
+ *          rule: 'default',   //必要性规则名
+ *          option: 20         //参数
+ *      }]
+ *      rules: [{               //规则集合
  *          rule: 'min',        //规则名
  *          option: 100         //校验条件
  *      }]
@@ -26,6 +30,7 @@ module.exports = (ruleObj = {}) => {
             if (! Array.isArray(params[key]) || params[key].length <= 1) continue;
             let ruleArray = params[key];
             let ruleList = [];
+            let nsList = [];
             for (let i=1, l=ruleArray.length; i<l; i++) {
                 let rule = ruleArray[i];
                 let option = null;
@@ -34,14 +39,15 @@ module.exports = (ruleObj = {}) => {
                 //校验规则条件
                 if (option) rules[rule](0, option);
                 let vl = {rule, option};
-                //优先检测条件
-                let preRules = ['required', 'required_if', 'required_unless', 'default', 'string', 'numeric', 'integer'];
-                if (preRules.includes(rule)) ruleList.unshift(vl);
+                //检测条件排序
+                if (['required', 'required_if', 'required_unless', 'default'].includes(rule)) nsList.push(vl);
+                else if (['string', 'numeric', 'integer'].includes(rule)) ruleList.unshift(vl);
                 else ruleList.push(vl);
             }
             options[key] = {
                 zh_CN: ruleArray[0],
                 en: key,
+                necessity: nsList,
                 rules: ruleList
             };
         }
@@ -58,6 +64,21 @@ module.exports = (ruleObj = {}) => {
             for (let key in keyMap) {
                 let comment = lang === 'en' ? keyMap[key]['en'] : keyMap[key]['zh_CN'];
                 let value = attributes[key];
+                //必要性检测
+                if (! value && value !== 0) {
+                    for (let {rule, option} of keyMap[key]['necessity']) {
+                        let result = rules[rule](value, option, attributes);
+                        if (! result[0]) {
+                            let err = `${comment}${language[lang][rule]}`;
+                            if (err.charAt(err.length - 1) === ' ') err += String(option);
+                            throw new ValidateError(err);
+                        } else if (result[1] !== null) {
+                            params[key] = value = result[1];
+                        }
+                    }
+                    continue;
+                }
+                //规则条件检测
                 for (let {rule, option} of keyMap[key]['rules']) {
                     let result = rules[rule](value, option, attributes);
                     if (! result[0]) {
